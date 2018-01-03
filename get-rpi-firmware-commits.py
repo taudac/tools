24a9d8f36f45673e6483dfc00b6bbf4765b2c136
Sergej Sawazki <ce3a@gmx.de>
2018-01-03 20:09:36 +0100

    firmware-commits: Add argparse boilerplate

--- get-rpi-firmware-commits.py
+++ get-rpi-firmware-commits.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python2.7
 import urllib2, json
+import argparse
 
 GITHUB_API_URL = 'https://api.github.com/repos'
 USER = 'Hexxeh'
 REPO = 'rpi-firmware'
 
-json_str = urllib2.urlopen(
-        "{}/{}/{}/commits".format(GITHUB_API_URL, USER, REPO)).read()
-commits = json.loads(json_str)
+def main(args):
+    json_str = urllib2.urlopen(
+            "{}/{}/{}/commits".format(GITHUB_API_URL, USER, REPO)).read()
+    commits = json.loads(json_str)
 
-for c in commits:
-    print c['sha'][0:8], c['commit']['message'].split('\n')[0]
+    for c in commits:
+        print c['sha'][0:8], c['commit']['message'].split('\n')[0]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    main(args)
