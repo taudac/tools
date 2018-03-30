0d2b3b507ea535dd41da50c1e7b7502269f10d26
Sergej Sawazki <ce3a@gmx.de>
2018-03-30 13:19:06 +0200

    build-modules.py: Sort the pending list by kernel version

--- build-modules.py
+++ build-modules.py
@@ -81,7 +81,7 @@ def main(cross_compile_args=""):
         return
 
     # download sources and build modules for each new kernel
-    for sha, kver in pending:
+    for sha, kver in sorted(pending, key=lambda x: x[1]):
         # download
         subprocess.check_call(["./get-rpi-kernel-sources.sh", sha])
         # remove old modules
